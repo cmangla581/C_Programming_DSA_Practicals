b62131fecbc367c2236dbd5f23adfbf4c16305f4
Chaitanya Mangla <tafscm@gmail.com>
2026-02-10 01:04:06 +0530

    Add files via upload

---
+++ Q1_DSA_1D_Array_Formation.c
@@ -0,0 +1,14 @@
+/* Write a C Programming to print the array and run the C Program online */ 
+
+# include <stdio.h> 
+
+int main(){
+    int i;
+    int a[5] = {1,2,3,4,5};
+    
+    for(i=0; i<5; i++){
+        printf("%d", a[i]);
+    }
+
+    return 0;  
+} 
